ef25e496d92423489a3606cfe6b93a4d07854734
natasha <singhnatasha752@gmail.com>
2024-08-01 13:53:32 +0530

    with new changes

--- pageObjects/SearchResultPage.py
+++ pageObjects/SearchResultPage.py
@@ -8,7 +8,7 @@ class SearchResultPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    SEARCH_FIRST_PRODUCT = (By.XPATH, "//span[@data-component-type='s-search-results']/div/div[2]")
+    SEARCH_FIRST_PRODUCT = (By.XPATH, "//span[@data-component-type='s-search-results']/div/div[4]")
 
     def click_first_product(self):
         self.find_element(*self.SEARCH_FIRST_PRODUCT).click()
