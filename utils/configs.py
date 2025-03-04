750f074433f40fd23f6aa945edbf140eebf3b18c
Natasha-debug <singhnatasha752@gmail.com>
2025-03-04 07:34:09 +0530

    Update configs.py
    hide the sensitive information.

--- utils/configs.py
+++ utils/configs.py
@@ -1,3 +1,3 @@
-BASE_URL = "https://www.amazon.com/?ref_=nav_ya_signin&captcha_verified=1&claim_type=EmailAddress&new_account=1&"
-USERNAME = "singhnatasha752@gmail.com"
-PASSWORD = "Tanya@05"
+BASE_URL = <URL OF WEBSITE>
+USERNAME = <LOGIN EMAIL ID>
+PASSWORD = <LOGIN PWD>
